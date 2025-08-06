4a6a8071f47a13b69c7ce7a212f41c92692c1e69
yashkumary385 <114864924+yashkumary385@users.noreply.github.com>
2025-08-06 13:24:38 +0530

    debugging

--- backend/routes/task.routes.js
+++ backend/routes/task.routes.js
@@ -1,17 +1,13 @@
-import { Router } from "express"
-import { createTask , getOneTask , getAllUserTasks , deleteTask , updateTask , fileDownload } from "../controllers/task.controller.js"
-import { verifyToken } from '../middlewares/auth.middleware.js';
-import { upload } from "../middlewares/multer.middleware.js";
-import { updateStatus } from "../controllers/task.controller.js";
-
-
 const router = Router();
-router.post("/",verifyToken,upload.array('attachments',3),createTask)
-router.get("/",verifyToken,getAllUserTasks)
-router.get("/:id", verifyToken,getOneTask)
-router.put("/:id",verifyToken,updateTask)
-router.delete("/:id",verifyToken,deleteTask)
-router.get("/download/:filename",verifyToken,fileDownload)
+
+router.get("/download/:filename", verifyToken, fileDownload);
 router.patch("/:id/status", verifyToken, updateStatus);
 
-export default router
+router.get("/", verifyToken, getAllUserTasks);
+router.get("/:id", verifyToken, getOneTask);
+router.put("/:id", verifyToken, updateTask);
+router.delete("/:id", verifyToken, deleteTask);
+
+router.post("/", verifyToken, upload.array('attachments', 3), createTask);
+
+export default router;
